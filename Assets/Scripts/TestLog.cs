8b5c40632bd0ab411d2b5912f4540435b2c37584
bluegonclass-creator <bluegonclass@gmail.com>
2026-03-18 04:33:29 +0900

    테스트 출력
    새로운 브랜치 생성

---
+++ Assets/Scripts/TestLog.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int t = 10;
+        Debug.Log($"Test{t}");
+    } 
+}
